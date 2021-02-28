d9856f6cdd983cdf9aa7bd4a5d1d9214576b5025
Molexar <nekita.potasev@mail.ru>
2021-02-28 12:03:16 +0300

    Added new file handlers with functions for handlers.go and in handlers.go was modified handler /currencies

---
+++ server/currency.go
@@ -0,0 +1,32 @@
+package server
+
+import "fmt"
+
+func (s *Server) createResponse(base string) (r *currencyResponse, err error) {
+	//Check if we has currencies
+	if !s.hasCurrencies {
+		return nil, fmt.Errorf("No currencies")
+	}
+
+	baserate, found := s.currencies[base]
+	if !found {
+		return nil, fmt.Errorf("Base string has no found")
+	}
+
+	//Create the response struct
+	response := currencyResponse{}
+	response.BaseCurrency = base
+	response.CurrencyDate = s.lastUpdateTime.format("2006-01-02")
+
+	// fill rates
+	for name, rate := range s.currencies {
+		relativeRate := rate / baserate
+		r := rateResponse{
+			Name: name,
+			Rate: relativeRate,
+		}
+		response.Rates = append(response.Rates, r)
+	}
+
+	return &response, nil
+}
--- server/haldlers.go
+++ server/haldlers.go
@@ -6,6 +6,17 @@ import (
 	"strings"
 )
 
+type currencyResponse struct {
+	CurrencyDate string         `json:"currency_date"`
+	BaseCurrency string         `json:"base_currency"`
+	Rates        []rateResponse `json:"rates"`
+}
+
+type rateResponse struct {
+	Name string  `json:"name"`
+	Rate float32 `json:"rate"`
+}
+
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	log.Printf("[%s]: %s\n", r.Method, r.RequestURI)
 
@@ -29,5 +40,14 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		} else {
 			http.NotFound(w, r)
 		}
+	}
+}
+
+//Handles request /currencies
+func (s *Server) currenciesHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		// Create response with EUR base
+		res, err := s.createResponse(eur)
+		s.respondJson(w, res, err)
 	}
 }
